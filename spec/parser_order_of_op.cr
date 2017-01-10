f8ede76d078ad5aabd6c4955c1f20d1bd6d1a83f
Virtual-Machine <darrelm@tech-asteroid.com>
2017-01-10 16:59:44 -0500

    Add spec test for mathematical expressions with parenthesis

--- spec/parser_order_of_op.cr
+++ spec/parser_order_of_op.cr
@@ -22,5 +22,35 @@ describe "Parser_2" do
 			program2.ast[0].children[0].children[0].children[1].class.should eq IntegerLiteralNode
 			program2.ast[0].children[0].children[0].children[1].value.should eq 2
 		end
+
+		input = "4 * ((3 + 2) + 1) + 5"
+
+		program3 = EmeraldProgram.new input
+		program3.lex
+		program3.parse
+
+		it "should handle parenthesis as sub expressions" do
+			program3.ast[0].children[0].class.should eq ExpressionNode
+			program3.ast[0].children[0].children[0].class.should eq BinaryOperatorNode
+			program3.ast[0].children[0].children[0].value.should eq "+"
+			program3.ast[0].children[0].children[0].children[0].class.should eq BinaryOperatorNode
+			program3.ast[0].children[0].children[0].children[0].value.should eq "*"
+			program3.ast[0].children[0].children[0].children[0].children[0].class.should eq IntegerLiteralNode
+			program3.ast[0].children[0].children[0].children[0].children[0].value.should eq 4
+			program3.ast[0].children[0].children[0].children[0].children[1].class.should eq ExpressionNode
+			program3.ast[0].children[0].children[0].children[0].children[1].children[0].class.should eq BinaryOperatorNode
+			program3.ast[0].children[0].children[0].children[0].children[1].children[0].value.should eq "+"
+			program3.ast[0].children[0].children[0].children[0].children[1].children[0].children[0].class.should eq ExpressionNode
+			program3.ast[0].children[0].children[0].children[0].children[1].children[0].children[0].children[0].class.should eq BinaryOperatorNode
+			program3.ast[0].children[0].children[0].children[0].children[1].children[0].children[0].children[0].value.should eq "+"
+			program3.ast[0].children[0].children[0].children[0].children[1].children[0].children[0].children[0].children[0].class.should eq IntegerLiteralNode
+			program3.ast[0].children[0].children[0].children[0].children[1].children[0].children[0].children[0].children[0].value.should eq 3
+			program3.ast[0].children[0].children[0].children[0].children[1].children[0].children[0].children[0].children[1].class.should eq IntegerLiteralNode
+			program3.ast[0].children[0].children[0].children[0].children[1].children[0].children[0].children[0].children[1].value.should eq 2
+			program3.ast[0].children[0].children[0].children[0].children[1].children[0].children[1].class.should eq IntegerLiteralNode
+			program3.ast[0].children[0].children[0].children[0].children[1].children[0].children[1].value.should eq 1
+			program3.ast[0].children[0].children[0].children[1].class.should eq IntegerLiteralNode
+			program3.ast[0].children[0].children[0].children[1].value.should eq 5
+		end
 	end
 end
