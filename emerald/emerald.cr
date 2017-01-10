38ccc6d15dd8367b634f4e2fe3bf1fa197e294e3
Virtual-Machine <darrelm@tech-asteroid.com>
2017-01-09 22:59:58 -0500

    Add current implementation of emerald

---
+++ emerald/emerald.cr
@@ -0,0 +1,46 @@
+require "./types"
+require "./token"
+require "./node"
+require "./lexer"
+require "./parser"
+
+class EmeraldProgram
+  getter lexer, parser, input_code, token_array, ast, output, delimiters
+
+  def initialize(@input_code : String)
+    @token_array = [] of Token
+    @ast = [] of Node
+    @output = ""
+    @lexer = Lexer.new ""
+  end
+
+  def lex
+    @lexer = Lexer.new input_code
+    @token_array = lexer.lex
+  end
+
+  def parse
+    @parser = Parser.new token_array
+    @ast = parser.not_nil!.parse
+  end
+
+  def generate
+  end
+
+  def compile
+    lex
+    parse
+    generate
+  end
+end
+
+input = "# I am a comment!
+four = 2 + 2
+puts four
+puts 10 < 6
+puts 11 != 10
+"
+
+program = EmeraldProgram.new input
+program.compile
+puts program.output
