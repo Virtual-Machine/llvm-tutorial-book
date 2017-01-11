66728fa5874eed395caf755c3d0e8dd51a24b706
Virtual-Machine <darrelm@tech-asteroid.com>
2017-01-11 16:49:17 -0500

    Add more spec tests for value resolution

--- spec/value_resolution.cr
+++ spec/value_resolution.cr
@@ -25,4 +25,25 @@ puts four - 3 * (four + 3)"
       second_expression.resolved_value.should eq -221
     end
   end
+
+  describe "value resolution_2" do
+    input = "
+four = (2 + 2) * (3 + 2 * (2 - 5)) + 2 - 8 * 3 * (5 + (6 * 7))
+puts four + 8 * 2 < four - 8 * 2
+"
+
+    program2 = EmeraldProgram.new input
+    program2.compile
+
+    first_expression = program2.ast[0].children[0]
+    second_expression = program2.ast[0].children[1]
+
+    it "resolves value of first expression as -1138" do
+      first_expression.resolved_value.should eq -1138
+    end
+
+    it "resolves value of second expression as false" do
+      second_expression.resolved_value.should eq false
+    end
+  end
 end
