09ac65f7039e539c350d2a5c6e69f7221d88fa68
Virtual-Machine <darrelm@tech-asteroid.com>
2017-01-11 23:11:18 -0500

    Implement rudimentary code generation, generating a valid LLVM IR file for puts calls.

--- emerald/emerald.cr
+++ emerald/emerald.cr
@@ -3,16 +3,28 @@ require "./token"
 require "./node"
 require "./lexer"
 require "./parser"
+require "./state"
+require "./instruction"
+
+require "llvm"
 
 class EmeraldProgram
-  getter lexer, parser, input_code, token_array, ast, output, delimiters
+  getter lexer, parser, input_code, token_array, ast, output, delimiters, state, mod, builder, main : LLVM::BasicBlock
+  getter! func : LLVM::Function
 
   def initialize(@input_code : String)
     @token_array = [] of Token
     @ast = [] of Node
     @output = ""
     @lexer = Lexer.new ""
-    @state = {} of String => ValueType
+    @state = ProgramState.new
+    @mod = LLVM::Module.new("Emerald")
+    @func = mod.functions.add "main", ([] of LLVM::Type), LLVM::Int32
+    func.linkage = LLVM::Linkage::External
+    @main = func.basic_blocks.append "main_body"
+    @builder = LLVM::Builder.new
+    state.add_function "main", func
+    state.add_function "puts", mod.functions.add "puts", [LLVM::VoidPointer], LLVM::Int32
   end
 
   def lex
@@ -26,7 +38,22 @@ class EmeraldProgram
   end
 
   def generate
-    @state = @ast[0].walk @state
+    @ast[0].walk state
+
+    builder.position_at_end main
+
+    state.instructions.each do |instruction|
+      instruction.build_instruction builder
+    end
+
+    # This should only be called if input source doesn't contain explicit return statement
+    # Requires a specific check during the ast walking stage to be implemented
+    builder.ret LLVM.int(LLVM::Int32, 0)
+
+    # To output an output.ll file ready to be converted to native assembly with llc
+    File.open("output.ll", "w") do |file|
+      mod.to_s(file)
+    end
   end
 
   def compile
