fe88354e6c6d1b50fafc7d1bcf0bf42530a62be0
Virtual-Machine <darrelm@tech-asteroid.com>
2017-01-14 17:08:03 -0500

    Improve error message output and condition checking

--- emerald/verifier.cr
+++ emerald/verifier.cr
@@ -10,24 +10,44 @@ class Verifier
 		pair_max = token_array.size - 2
 		token_array.each_with_index do |token, index|
 			if index <= pair_max
-				next_token = token_array[index + 1]
 				
-				if token.typeT == TokenType::Operator && next_token.typeT == TokenType::Operator
-					emerald_syntax_error token.line, token.column, "Two tokens   #{token.value}   #{next_token.value}   were detected in sequence, this is not valid Emerald syntax"
-				end
 				
-				if token.typeT == TokenType::ParenOpen && next_token.typeT == TokenType::Operator
-					emerald_syntax_error token.line, token.column, "Two tokens   #{token.value}   #{next_token.value}   were detected in sequence, this is not valid Emerald syntax"
+				if token.typeT == TokenType::String
+					token_value = "\"#{token.value}\""
+				else
+					token_value = token.value
 				end
 
-				if token.typeT == TokenType::Operator && next_token.typeT == TokenType::ParenClose
-					emerald_syntax_error token.line, token.column, "Two tokens   #{token.value}   #{next_token.value}   were detected in sequence, this is not valid Emerald syntax"
-				end				
+				next_token = token_array[index + 1]
+				if next_token.typeT == TokenType::String
+					next_token_value = "\"#{next_token.value}\""
+				else
+					next_token_value = next_token.value
+				end
+
+				error_string = "Two tokens   #{token_value}   #{next_token_value}   were detected in sequence, this is not valid Emerald syntax"
+				error = EmeraldSyntaxException.new error_string, token.line, token.column
+				case token.typeT
+				when TokenType::Operator
+					if next_token.typeT == TokenType::Operator || next_token.typeT == TokenType::ParenClose
+						raise error
+					end
+				when TokenType::ParenOpen
+					if next_token.typeT == TokenType::Operator
+						raise error
+					end
+				when TokenType::ParenClose
+					case next_token.typeT
+					when TokenType::Int, TokenType::Float, TokenType::String, TokenType::Bool, TokenType::Symbol, TokenType::ParenOpen
+						raise error
+					end
+				when TokenType::Int, TokenType::Float, TokenType::String, TokenType::Bool, TokenType::Symbol
+					case next_token.typeT
+					when TokenType::Int, TokenType::Float, TokenType::String, TokenType::Bool, TokenType::Symbol, TokenType::ParenOpen
+						raise error
+					end
+				end	
 			end
 		end
-	end
-
-	def emerald_syntax_error(line, column, message)
-		raise EmeraldSyntaxException.new message, line, column
 	end
 end
