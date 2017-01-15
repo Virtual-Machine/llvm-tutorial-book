01eea016a50d9e03837a4e398041d602cabe3ca2
Virtual-Machine <darrelm@tech-asteroid.com>
2017-01-14 20:32:55 -0500

    Add tests to ensure exceptions are raised for given syntax problems.

---
+++ spec/errors.cr
@@ -0,0 +1,65 @@
+require "spec"
+require "../emerald/emerald"
+
+# Note that these examples create program examples with:
+#      EmeraldProgram.new input_code, true
+# The true param forces program to re-raise exception instead of printing and exiting to allow tests to work as expected.
+
+describe "Errors" do
+	describe "Emerald error handling" do
+		it "should catch bad combinations of tokens 1" do
+			expect_raises EmeraldTokenVerificationException do
+				program = EmeraldProgram.new "puts ( * 2)", true
+				program.compile
+			end
+		end
+
+		it "should catch bad combinations of tokens 2" do
+			expect_raises EmeraldTokenVerificationException do
+				program = EmeraldProgram.new "puts (2 *)", true
+				program.compile
+			end
+		end
+
+		it "should catch bad combinations of tokens 3" do
+			expect_raises EmeraldTokenVerificationException do
+				program = EmeraldProgram.new "puts 2 * * 2", true
+				program.compile
+			end
+		end
+		it "should catch bad combinations of tokens 4" do
+			expect_raises EmeraldTokenVerificationException do
+				program = EmeraldProgram.new "puts 2 ()", true
+				program.compile
+			end
+		end
+
+		it "should catch bad combinations of tokens 5" do
+			expect_raises EmeraldTokenVerificationException do
+				program = EmeraldProgram.new "puts () 2", true
+				program.compile
+			end
+		end
+
+		it "should catch bad combinations of tokens 6" do
+			expect_raises EmeraldTokenVerificationException do
+				program = EmeraldProgram.new "puts 2 \"Int\"", true
+				program.compile
+			end
+		end
+
+		it "should cause value resolution errors for undefined operators on known type combinations" do
+			expect_raises EmeraldValueResolutionException do
+				program = EmeraldProgram.new "puts 1 += 1", true
+				program.compile
+			end
+		end
+
+		it "should cause value resolution errors for undefined type combinations" do
+			expect_raises EmeraldValueResolutionException do
+				program = EmeraldProgram.new "puts true + 1", true
+				program.compile
+			end
+		end
+	end
+end
