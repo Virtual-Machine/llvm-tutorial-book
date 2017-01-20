1caa4e9cfd091d6bae1192cb473257a6b7b31f45
Virtual-Machine <darrelm@tech-asteroid.com>
2017-01-20 14:46:10 -0500

    Correct strings spec to align with new api

--- spec/strings.cr
+++ spec/strings.cr
@@ -2,42 +2,10 @@ require "spec"
 require "../emerald/emerald"
 
 describe "String processing" do
-  input = "
-# puts string
-puts \"Hello from this file\"
+  system "./emeraldc test_inputs/input4.cr -e > test_outputs/output4"
+  contents = File.read("test_outputs/output4")
 
-# string addition is concatenation
-puts \"Hello\" + \" world!\"
-
-# string multiplied by integer is repetition
-puts \"Repeated \" * 3 + \"!\"
-
-# strings can be equal or inqual to other strings
-puts \"Hello\" == \"Hello\"
-puts \"Hello\" != \"Hello\"
-puts \"Hello\" == \"Hello1\"
-puts \"Hello\" != \"Hello1\"
-"
-
-  program = EmeraldProgram.new input
-  program.compile
-
-  # This needs fixing as instructions are no longer generated
-  # it "should resolve seven puts calls with desired string output" do
-  #   program.state.instructions[0].class.should eq CallInstruction
-  #   program.state.instructions[0].as(CallInstruction).params[0].should eq LLVM.string("Hello from this file")
-  #   program.state.instructions[1].class.should eq CallInstruction
-  #   program.state.instructions[1].as(CallInstruction).params[0].should eq LLVM.string("Hello world!")
-  #   program.state.instructions[2].class.should eq CallInstruction
-  #   program.state.instructions[2].as(CallInstruction).params[0].should eq LLVM.string("Repeated Repeated Repeated !")
-  #   program.state.instructions[3].class.should eq CallInstruction
-  #   program.state.instructions[3].as(CallInstruction).params[0].should eq LLVM.string("true")
-  #   program.state.instructions[4].class.should eq CallInstruction
-  #   program.state.instructions[4].as(CallInstruction).params[0].should eq LLVM.string("false")
-  #   program.state.instructions[5].class.should eq CallInstruction
-  #   program.state.instructions[5].as(CallInstruction).params[0].should eq LLVM.string("false")
-  #   program.state.instructions[6].class.should eq CallInstruction
-  #   program.state.instructions[6].as(CallInstruction).params[0].should eq LLVM.string("true")
-  # end
-
+  it "should resolve seven puts calls with desired string output" do
+    contents.should eq "Hello from this file\nHello world!\nRepeated Repeated Repeated !\ntrue\nfalse\nfalse\ntrue\n"
+  end
 end
---
+++ test_inputs/input4.cr
@@ -0,0 +1,14 @@
+# puts string
+puts "Hello from this file"
+
+# string addition is concatenation
+puts "Hello" + " world!"
+
+# string multiplied by integer is repetition
+puts "Repeated " * 3 + "!"
+
+# strings can be equal or inqual to other strings
+puts "Hello" == "Hello"
+puts "Hello" != "Hello"
+puts "Hello" == "Hello1"
+puts "Hello" != "Hello1"
