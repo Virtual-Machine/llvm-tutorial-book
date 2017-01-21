1a041255c3aed3c9ee62eab2d22b353494a48f7c
Virtual-Machine <darrelm@tech-asteroid.com>
2017-01-20 19:56:04 -0500

    Add spec for binary operations between variables and literals

---
+++ spec/variables_and_literals.cr
@@ -0,0 +1,11 @@
+require "spec"
+require "../emerald/emerald"
+
+describe "Generator" do
+  system "./emeraldc test_inputs/input7.cr -e > test_outputs/output7"
+  contents = File.read("test_outputs/output7")
+
+  it "should resolve final output as expected" do
+    contents.should eq "8.300000\n8.300000\n8.300000\n8.300000\n8.300000\n8.300000\ntrue\nfalse\ntrue\nfalse\ntrue\nfalse\nfalse\ntrue\nfalse\ntrue\nfalse\ntrue\nfalse\nfalse\nfalse\nfalse\nfalse\nfalse\ntrue\ntrue\ntrue\ntrue\ntrue\ntrue\ntrue\nfalse\ntrue\nfalse\ntrue\nfalse\n"
+  end
+end
---
+++ test_inputs/input7.cr
@@ -0,0 +1,46 @@
+int_var = 4
+str_var = "String"
+float_var = 4.3
+bool_var = true
+
+puts int_var + float_var
+puts float_var + int_var
+puts 4 + float_var
+puts float_var + 4
+puts int_var + 4.3
+puts 4.3 + int_var
+
+puts int_var < float_var
+puts float_var < int_var
+puts 4 < float_var
+puts float_var < 4
+puts int_var < 4.3
+puts 4.3 < int_var
+
+puts int_var > float_var
+puts float_var > int_var
+puts 4 > float_var
+puts float_var > 4
+puts int_var > 4.3
+puts 4.3 > int_var
+
+puts int_var == float_var
+puts float_var == int_var
+puts 4 == float_var
+puts float_var == 4
+puts int_var == 4.3
+puts 4.3 == int_var
+
+puts int_var != float_var
+puts float_var != int_var
+puts 4 != float_var
+puts float_var != 4
+puts int_var != 4.3
+puts 4.3 != int_var
+
+puts "String" == str_var
+puts "String" != str_var
+puts str_var == "String"
+puts str_var != "String"
+puts str_var == str_var
+puts str_var != str_var
