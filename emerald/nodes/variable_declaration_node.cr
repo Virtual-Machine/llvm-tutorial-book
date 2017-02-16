b7dfd785b247b956f0e969352e8926bca4f85d0d
Virtual-Machine <darrelm@tech-asteroid.com>
2017-02-16 15:58:23 -0500

    Correct variable declaration value resolution to use pointer if available

--- emerald/nodes/variable_declaration_node.cr
+++ emerald/nodes/variable_declaration_node.cr
@@ -5,7 +5,7 @@ class VariableDeclarationNode < Node
 
   def resolve_value(state : ProgramState) : Nil
     @resolved_value = @children[0].resolved_value
-    if state.variable_exists? state.active_function, @value.as(String)
+    if state.pointer_exists? state.active_function, @value.as(String)
       state.builder.position_at_end state.active_block
       state.builder.store (crystal_to_llvm state, @resolved_value), state.variable_pointers[state.active_function][@value.as(String)]
     end
