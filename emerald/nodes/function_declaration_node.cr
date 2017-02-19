508d2d780976abb22920effbc10fdbd207ce250b
Virtual-Machine <darrelm@tech-asteroid.com>
2017-02-18 21:39:40 -0500

    Correct and expand function declaration value resolution

--- emerald/nodes/function_declaration_node.cr
+++ emerald/nodes/function_declaration_node.cr
@@ -25,12 +25,73 @@ class FunctionDeclarationNode < Node
   end
 
   def resolve_value(state : ProgramState) : Nil
-    if @children[-1].class != ReturnNode
+    if @children[0].children[-1].class != ReturnNode
       state.builder.position_at_end state.active_block
-      # TODO : implement forced returns from last child for all major types
       case @return_type
       when :Nil
         state.builder.ret
+      when :Int32
+        if @children[0].children[-1].resolved_value.is_a?(Int32)
+          state.builder.ret LLVM.int(LLVM::Int32, @children[0].children[-1].resolved_value.as(Int32))
+        elsif @children[0].children[-1].resolved_value.is_a?(LLVM::Value)
+          if @children[0].children[-1].resolved_value.as(LLVM::Value).type == LLVM::Int32
+            state.builder.ret @children[0].children[-1].resolved_value.as(LLVM::Value)
+          else
+            raise "#{@name} function requires an explicit or implicit integer return"
+          end
+        else
+          raise "#{@name} function requires an explicit or implicit integer return"
+        end
+      when :Int64
+        if @children[0].children[-1].resolved_value.is_a?(LLVM::Value)
+          if @children[0].children[-1].resolved_value.as(LLVM::Value).type == LLVM::Int64
+            state.builder.ret @children[0].children[-1].resolved_value.as(LLVM::Value)
+          else
+            raise "#{@name} function requires an explicit or implicit integer64 return"
+          end
+        else
+          raise "#{@name} function requires an explicit or implicit integer64 return"
+        end
+      when :Float64
+        if @children[0].children[-1].resolved_value.is_a?(Float64)
+          state.builder.ret LLVM.double(@children[0].children[-1].resolved_value.as(Float64))
+        elsif @children[0].children[-1].resolved_value.is_a?(LLVM::Value)
+          if @children[0].children[-1].resolved_value.as(LLVM::Value).type == LLVM::Double
+            state.builder.ret @children[0].children[-1].resolved_value.as(LLVM::Value)
+          else
+            raise "#{@name} function requires an explicit or implicit float return"
+          end
+        else
+          raise "#{@name} function requires an explicit or implicit float return"
+        end
+      when :Bool
+        if @children[0].children[-1].resolved_value.is_a?(Bool)
+          if @children[0].children[-1].resolved_value.as(Bool) == true
+            state.builder.ret LLVM.int(LLVM::Int1, 1)
+          else
+            state.builder.ret LLVM.int(LLVM::Int1, 0)
+          end
+        elsif @children[0].children[-1].resolved_value.is_a?(LLVM::Value)
+          if @children[0].children[-1].resolved_value.as(LLVM::Value).type == LLVM::Int1
+            state.builder.ret @children[0].children[-1].resolved_value.as(LLVM::Value)
+          else
+            raise "#{@name} function requires an explicit or implicit bool return"
+          end
+        else
+          raise "#{@name} function requires an explicit or implicit bool return"
+        end
+      when :String
+        if @children[0].children[-1].resolved_value.is_a?(String)
+          state.builder.ret state.define_or_find_global @children[0].children[-1].resolved_value.as(String)
+        elsif @children[0].children[-1].resolved_value.is_a?(LLVM::Value)
+          if @children[0].children[-1].resolved_value.as(LLVM::Value).type == LLVM::Int8.pointer
+            state.builder.ret @children[0].children[-1].resolved_value.as(LLVM::Value)
+          else
+            raise "#{@name} function requires an explicit or implicit bool return"
+          end
+        else
+          raise "#{@name} function requires an explicit or implicit bool return"
+        end
       end
     end
     state.active_function = state.mod.functions["main"]
