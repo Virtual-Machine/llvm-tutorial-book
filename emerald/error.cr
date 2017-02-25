7becfa241a1c0639f0c7359e533dc0ce54d888a7
Virtual-Machine <darrelm@tech-asteroid.com>
2017-02-25 15:28:10 -0500

    Refactor full_error method for readability

--- emerald/error.cr
+++ emerald/error.cr
@@ -16,29 +16,16 @@ class EmeraldSyntaxException < Exception
     if test_mode
       raise self
     else
-      if color
-        puts self.to_s_coloured
-      else
-        puts self.to_s
-      end
-
+      color ? puts self.to_s_coloured : puts self.to_s
       puts
 
       lines = source.split("\n")
-      print_pre_line = true
-      print_post_line = true
-      if self.line == 1
-        print_pre_line = false
-      end
-      if self.line == lines.size
-        print_post_line = false
-      end
+        
+      print_pre_line = self.line == 1 ? false : true
+      print_post_line = self.line == lines.size ? false : true
+      
       puts "Line #{self.line - 1} : #{lines[self.line - 2].strip}" if print_pre_line
-      if color
-        puts "\033[031mLine #{self.line} :\033[039m #{lines[self.line - 1].strip}"
-      else
-        puts "Line #{self.line} : #{lines[self.line - 1].strip}"
-      end
+      color ? puts "\033[031mLine #{self.line} :\033[039m #{lines[self.line - 1].strip}" : puts "Line #{self.line} : #{lines[self.line - 1].strip}"
       puts "Line #{self.line + 1} : #{lines[self.line].strip}" if print_post_line
       exit 1
     end
